f6d026a200852261bc2dea7dd0232b4c87046c6e
PixCap <pixcapsoft@gmail.com>
2026-03-30 12:07:31 +0530

    Updated model & textures

--- src/main/java/lk/pixcapsoft/deadpoint/block/DeadPointGravestone.java
+++ src/main/java/lk/pixcapsoft/deadpoint/block/DeadPointGravestone.java
@@ -1,23 +1,18 @@
 package lk.pixcapsoft.deadpoint.block;
 
 import lk.pixcapsoft.deadpoint.DeadPoint;
-import net.minecraft.core.BlockPos;
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.Identifier;
 import net.minecraft.resources.ResourceKey;
-import net.minecraft.server.level.ServerLevel;
-import net.minecraft.world.entity.Display;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.fabricmc.fabric.api.creativetab.v1.CreativeModeTabEvents;
 import net.minecraft.world.item.CreativeModeTabs;
-import net.minecraft.world.level.block.state.BlockState;
 
 import java.util.function.Function;
 
